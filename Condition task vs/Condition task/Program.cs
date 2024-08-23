dd4506518530771bc08e1a1b2629da8b029d8709
Asifmukhtarov <muxtarovasif123@gmail.com>
2024-08-23 22:57:59 +0400

    tasks was added

---
+++ Condition task vs/Condition task/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Task 1
+*int num = -25;
+if (num > 0)
+{
+    Console.WriteLine("eded musbetdir");
+}
+else
+{
+    Console.WriteLine("eded menfidir");
+}
+*/
+/*Task 2
+int num = 21;
+if (num % 3 !=0 && num % 7 !=0)
+{
+    Console.WriteLine("Eded 3 ve 7 reqemine tam bolunmur ");
+}
+else
+{
+    Console.WriteLine("Eded 3 ve 7 reqemine tam bolunur");
+}*/ 
+/*Task 3
+int num = 53;
+int teklik = num % 10;
+int onluq = num / 10;
+if (teklik > onluq  )
+{
+    Console.WriteLine(teklik);
+}
+else if ( teklik == onluq)
+{
+    Console.WriteLine("mertebeler bir birine beraberdir");
+}
+else
+{
+     Console.WriteLine(onluq);
+}*/
